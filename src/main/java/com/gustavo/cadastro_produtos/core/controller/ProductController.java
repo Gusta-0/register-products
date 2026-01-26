92ab79fc17c15317064539f783a58ccfc2b75791
Gustavo Alves <gabs.principal.2005@gmail.com>
2026-01-26 15:26:25 -0300

    :goal_net: feat: aplicar tratamento de exceções padronizado

--- src/main/java/com/gustavo/cadastro_produtos/core/controller/ProductController.java
+++ src/main/java/com/gustavo/cadastro_produtos/core/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.gustavo.cadastro_produtos.core.controller;
 import com.gustavo.cadastro_produtos.core.service.ProductService;
 import com.gustavo.cadastro_produtos.dto.request.ProductRequest;
 import com.gustavo.cadastro_produtos.dto.response.ProductResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +21,9 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponse> save(@RequestBody ProductRequest request) {
-        ProductResponse response = productService.saveProduct(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    public ResponseEntity<ProductResponse> save(@Valid @RequestBody ProductRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(productService.saveProduct(request));
     }
 
     @GetMapping
@@ -30,25 +31,26 @@ public class ProductController {
         return ResponseEntity.ok(productService.findAll());
     }
 
-    @GetMapping("/id/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> findById(@PathVariable Long id) {
         return ResponseEntity.ok(productService.findById(id));
     }
 
-    @GetMapping("/name/{name}")
+    @GetMapping("/{name}")
     public ResponseEntity<ProductResponse> findByName(@PathVariable String name) {
         return ResponseEntity.ok(productService.findByName(name));
     }
 
-    @PutMapping("/id/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ProductResponse> update(
+            @Valid
             @PathVariable Long id,
             @RequestBody ProductRequest request
     ) {
         return ResponseEntity.ok(productService.update(id, request));
     }
 
-    @DeleteMapping("/id/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         productService.delete(id);
         return ResponseEntity.noContent().build();

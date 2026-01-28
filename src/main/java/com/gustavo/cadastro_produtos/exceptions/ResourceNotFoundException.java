28d86201eea29f2cab30eeaa92917ba5ff461820
Gustavo Alves <gabs.principal.2005@gmail.com>
2026-01-28 16:30:41 -0300

    🗑️ chore(cleanup): Remove exceção não utilizada do projeto

--- src/main/java/com/gustavo/cadastro_produtos/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gustavo.cadastro_produtos.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-

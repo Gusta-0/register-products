905d591298b20a677586fddbeacc602f4421f7fd
Gustavo Alves <gabs.principal.2005@gmail.com>
2026-02-03 10:54:46 -0300

    refactor: remove comentários desnecessários 🧹

--- src/main/java/com/gustavo/cadastro_produtos/core/service/NormalPriceCalculator.java
+++ src/main/java/com/gustavo/cadastro_produtos/core/service/NormalPriceCalculator.java
@@ -3,7 +3,6 @@ package com.gustavo.cadastro_produtos.core.service;
 import com.gustavo.cadastro_produtos.core.entity.Product;
 
 import java.math.BigDecimal;
-//Interfaces segregadas (princípio da Segregação de Interfaces do SOLID)
 
 public class NormalPriceCalculator implements PriceCalculator{
     @Override

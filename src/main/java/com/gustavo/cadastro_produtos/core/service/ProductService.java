7e920ba583bda15c1e937fcd8bab248478ea8016
Gustavo Alves <gabs.principal.2005@gmail.com>
2026-02-05 16:31:51 -0300

    ⚡ feat(service): atualizar método findAll em ProductService

--- src/main/java/com/gustavo/cadastro_produtos/core/service/ProductService.java
+++ src/main/java/com/gustavo/cadastro_produtos/core/service/ProductService.java
@@ -1,16 +1,17 @@
 package com.gustavo.cadastro_produtos.core.service;
 
-
-
 import com.gustavo.cadastro_produtos.dto.request.ProductRequest;
 import com.gustavo.cadastro_produtos.dto.request.ProductUpdateRequest;
 import com.gustavo.cadastro_produtos.dto.response.ProductResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductService {
     ProductResponse saveProduct(ProductRequest request);
-    List<ProductResponse> findAll();
+    Page<ProductResponse> findAll(Pageable pageable);
     ProductResponse findById(Long id);
     ProductResponse update(Long id, ProductUpdateRequest request);
     void delete(Long id);

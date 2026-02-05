13d64d02cd7ac92777d3d9009f365100c1269eea
Gustavo Alves <gabs.principal.2005@gmail.com>
2026-02-05 16:30:26 -0300

    🧹 chore: remover 4 classes obsoletas

--- src/main/java/com/gustavo/cadastro_produtos/core/service/PriceCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.gustavo.cadastro_produtos.core.service;
-
-import com.gustavo.cadastro_produtos.core.entity.Product;
-import java.math.BigDecimal;
-
-public interface PriceCalculator {
-    BigDecimal calculate(Product product);
-}
